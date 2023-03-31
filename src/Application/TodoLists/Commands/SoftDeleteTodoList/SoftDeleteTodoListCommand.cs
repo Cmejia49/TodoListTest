4a67abc036baeeb1c2e2004d2b733e133f66da40
christopher.mejia@zoobooksystems.com <christopher.mejia@zoobooksystems.com>
2023-03-31 19:49:30 +0800

    feature3 done

---
+++ src/Application/TodoLists/Commands/SoftDeleteTodoList/SoftDeleteTodoListCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Todo_App.Application.Common.Exceptions;
+using Todo_App.Application.Common.Interfaces;
+using Todo_App.Domain.Entities;
+
+namespace Todo_App.Application.TodoLists.Commands.SoftDeleteTodoList;
+public record SoftDeleteTodoListCommand(int Id) : IRequest;
+
+public class SoftDeleteTodoListCommandHandler : IRequestHandler<SoftDeleteTodoListCommand>
+{
+    private readonly IApplicationDbContext _context;
+
+    public SoftDeleteTodoListCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Unit> Handle(SoftDeleteTodoListCommand request, CancellationToken cancellationToken)
+    {
+        var entity = await _context.TodoLists
+            .FindAsync(new object[] { request.Id }, cancellationToken);
+
+        if (entity == null)
+        {
+            throw new NotFoundException(nameof(TodoLists), request.Id);
+        }
+
+       var todoItem = await _context.TodoItems.Where(x => x.ListId == request.Id).ToListAsync();
+        foreach (var item in todoItem)
+        {
+           item.Status = 1;
+        }
+        entity.Status = 1;
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return Unit.Value;
+    }
+}
---
+++ src/Infrastructure/Persistence/Migrations/20230331084601_AlterTableMigration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Todo_App.Infrastructure.Persistence.Migrations
+{
+    public partial class AlterTableMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "TodoLists",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "TodoItems",
+                type: "int",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "TodoLists");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "TodoItems");
+        }
+    }
+}
