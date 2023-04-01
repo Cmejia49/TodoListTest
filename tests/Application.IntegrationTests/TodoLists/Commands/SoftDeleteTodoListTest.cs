fddc069be47759a8cfdbd10aa56a67938cd90d30
cmejia49 <mejiachristopher49@gmail.com>
2023-04-02 00:33:15 +0800

    add soft delete test

---
+++ tests/Application.IntegrationTests/TodoLists/Commands/SoftDeleteTodoListTest.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Todo_App.Application.Common.Exceptions;
+using Todo_App.Application.TodoLists.Commands.CreateTodoList;
+using Todo_App.Application.TodoLists.Commands.SoftDeleteTodoList;
+using Todo_App.Domain.Entities;
+
+namespace Todo_App.Application.IntegrationTests.TodoLists.Commands;
+
+using static Testing;
+public class SoftDeleteTodoListTest : BaseTestFixture
+{
+
+    [Test]
+    public async Task ShouldRequireValidTodoItemId()
+    {
+        var command = new SoftDeleteTodoListCommand(99);
+
+        await FluentActions.Invoking(() =>
+            SendAsync(command)).Should().ThrowAsync<NotFoundException>();
+    }
+
+    [Test]
+    public async Task ShouldDeleteTodoItem()
+    {
+        var listId = await SendAsync(new CreateTodoListCommand
+        {
+            Title = "New List"
+        });
+
+        await SendAsync(new SoftDeleteTodoListCommand(listId));
+
+        var list = await FindAsync<TodoList>(listId);
+
+        list.Should().BeNull();
+    }
+}
