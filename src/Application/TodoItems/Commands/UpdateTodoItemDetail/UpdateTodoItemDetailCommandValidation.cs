471dd9447be2203e79c4e09345b7af904ae3b1ac
cmejia49 <mejiachristopher49@gmail.com>
2023-04-02 00:26:46 +0800

    add test

---
+++ src/Application/TodoItems/Commands/UpdateTodoItemDetail/UpdateTodoItemDetailCommandValidation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Todo_App.Application.Common.Interfaces;
+
+namespace Todo_App.Application.TodoItems.Commands.UpdateTodoItemDetail;
+public class UpdateTodoItemDetailCommandValidation : AbstractValidator<UpdateTodoItemDetailCommand>
+{
+
+    public UpdateTodoItemDetailCommandValidation()
+    {
+
+        RuleFor(v => v.ItemColour)
+       .Must(BeValidHexColor).WithMessage("Color Value Invalid.");
+    }
+
+    private bool BeValidHexColor(string? color)
+    {
+        return Regex.Match(color, "^#[0-9a-fA-F]{6}$").Success;
+    }
+
+
+}
